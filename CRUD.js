f3a7355e4ea6ffb8723a9270ef44a3beb5a8616e
hoang2001 <hoangdzai1131@gmail.com>
2023-03-04 21:04:15 +0700

    upload code

---
+++ CRUD.js
@@ -0,0 +1,88 @@
+const linkRoot = "http://localhost:3000/product";
+function getByAjax() {
+  $.ajax({
+    url: `${linkRoot}`,
+    type: "get",
+    dataType: "json",
+    success: function (data) {
+      render(data);
+    },
+  });
+}
+getByAjax();
+
+const tbody = document.querySelector("tbody");
+function render(data) {
+  let html = data.map((sp) => {
+    return `<tr>
+          <td class='id'>${sp.id}</td>
+          <td class='name'>${sp.name}</td>
+          <td class='img'>${sp.img}</td>
+          <td class='des'>${sp.des}</td>
+          <td class='action'><button onClick ='handleDelete(${sp.id})' class="btn btn-outline-danger">delete</button> 
+          <button onClick ='handleEdit(${sp.id})' class="btn btn-outline-info">edit</button></td>
+          </tr>`;
+  });
+  tbody.innerHTML = html.join("");
+  value_img = "";
+  value_name = "";
+  value_des = "";
+}
+var item_img = document.querySelector("#img");
+var item_name = document.querySelector("#name");
+var item_des = document.querySelector("#des");
+function handleAdd() {
+  $.ajax({
+    url: `${linkRoot}`,
+    data: {
+      name: item_name.value,
+      img: item_img.value,
+      des: item_des.value,
+    },
+    type: "post",
+    dataType: "json",
+    success: function (res) {
+      getByAjax();
+    },
+  });
+}
+function handleDelete(id) {
+  $.ajax({
+    url: `${linkRoot}/${id}`,
+    type: "delete",
+    dataType: "json",
+    success: function (data) {
+      getByAjax();
+    },
+  });
+}
+let z = 0;
+function handleEdit(id) {
+  z = id;
+  $.ajax({
+    url: `${linkRoot}` + `/` + id,
+    type: "get",
+    dataType: "json",
+    success: function (res) {
+      item_name.value = res.name;
+      item_img.value = res.img;
+      item_des.value = res.des;
+    },
+  });
+}
+
+function handleUpdate() {
+  $.ajax({
+    url: `${linkRoot}/` + z,
+    data: {
+      name: item_name.value,
+      img: item_img.value,
+      des: item_des.value,
+    },
+    type: "put",
+    dataType: "json",
+    success: function (res) {
+      getByAjax();
+    },
+  });
+}
---
+++ product.js
@@ -0,0 +1,26 @@
+const linkRoot = "http://localhost:3000/product";
+function getByAjax() {
+  $.ajax({
+    url: `${linkRoot}`,
+    type: "get",
+    dataType: "json",
+    success: function (res) {
+      render(res);
+    },
+  });
+}
+getByAjax();
+const card = document.querySelector("#card1");
+function render(data) {
+  let html = data.map((item) => {
+    return `<div class="col-3 card mt-5">
+    <img src="${item.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title" >${item.name}</h5>
+      <p class="card-text">${item.des}</p>
+    </div>
+    <a href="#" class="btn btn-primary" >xem them</a>
+  </div>`;
+  });
+  card.innerHTML = html.join("");
+}
